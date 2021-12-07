114c855ceb3c453d13abd3644c5def9e5f917b8d
manhtq <manhtq@sunshinegroup.vn>
2021-12-07 18:02:24 +0700

    sp

--- convert-docx-to-posts/backend/views/admin.php
+++ convert-docx-to-posts/backend/views/admin.php
@@ -9,35 +9,25 @@
  * @author    Trinh Quoc manh <trinhquocmanh1990@gmail.com>
  * @copyright 2020
  * @license   GPL 2.0+
- * @link      
+ * @link
  */
 ?>
 
 <div class="wrap">
 
-	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+    <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
 
-	<div id="tabs" class="settings-tab">
-		<ul>
-			<li><a href="#tabs-1"><?php esc_html_e( 'Settings', CDTP_TEXTDOMAIN ); ?></a></li>
-			<li><a href="#tabs-2"><?php esc_html_e( 'Settings 2', CDTP_TEXTDOMAIN ); ?></a></li>
-			<?php
-			?>
-		</ul>
-		<?php
-		require_once plugin_dir_path( __FILE__ ) . 'settings.php';
-		require_once plugin_dir_path( __FILE__ ) . 'settings-2.php';
-		?>
-		<?php
-		?>
-	</div>
+    <div id="tabs" class="settings-tab">
+        <ul>
+            <li><a href="#tabs-1"><?php esc_html_e('Settings', CDTP_TEXTDOMAIN); ?></a></li>
+            <?php
+            ?>
+        </ul>
+        <?php
+        require_once plugin_dir_path(__FILE__) . 'settings.php';
+        ?>
+        <?php
+        ?>
+    </div>
 
-	<div class="right-column-settings-page metabox-holder">
-		<div class="postbox">
-			<h3 class="hndle"><span><?php esc_html_e( 'Plugin Name.', CDTP_TEXTDOMAIN ); ?></span></h3>
-			<div class="inside">
-				<a href="https://github.com/WPBP/WordPress-Plugin-Boilerplate-Powered"><img src="https://raw.githubusercontent.com/WPBP/boilerplate-assets/master/icon-256x256.png" alt=""></a>
-			</div>
-		</div>
-	</div>
 </div>
